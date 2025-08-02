2b0ed23db5a67ed3e60be54ce34b013e98f57983
C V CHARAN <cherryiiit1234@gmail.com>
2025-08-02 23:31:44 +0530

    initial commit with frontnedn and backend

---
+++ backend/src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Error", err);
+    process.exit(1);
+  }
+};
+export const disconnectDB = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log("MongoDB Disconnected");
+  } catch (err) {
+    console.error("MongoDB Disconnection Error", err);
+  }
+};
