c5361021301b20ceeaf734647e491e66f96e22ff
C V CHARAN <cherryiiit1234@gmail.com>
2025-08-03 19:28:42 +0530

    feat : add morgan for loggin in the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,13 @@ import cors from 'cors';
 import authRouter from './routes/auth.routes';
 import postRouter from './routes/post.routes';
 import profileRouter from './routes/profile.routes';
+import morgan from 'morgan';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
+
 
 app.use('/api/auth', authRouter);
 app.use('/api/post', postRouter);
